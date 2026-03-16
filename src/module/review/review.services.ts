928478c71a88bbd2cc89cd7bf55a7f846c90390c
fuadhossainnishad <fuadhossainbk01@gmail.com>
2026-03-17 02:55:26 +0600

    resolve review issue

--- src/module/review/review.services.ts
+++ src/module/review/review.services.ts
@@ -1,67 +1,120 @@
 import { idConverter } from "../../utility/idConverter";
 import { buildMeta, calculatePagination, IAggregationResponse } from "../stats/stats.services";
-import User from "../user/user.model";
 import Review from "./review.model";
 
-const getReviewService = async <T>(productId: string, query: Record<string, unknown>): Promise<IAggregationResponse<T>> => {
+const getReviewService = async <T>(
+  productId: string,
+  query: Record<string, unknown>
+): Promise<IAggregationResponse<T>> => {
   const { page, limit, skip } = calculatePagination(query);
-
-
   const productIdObject = await idConverter(productId);
-  console.log("Converted ProductId: ", productIdObject);
 
-  const review = await Review.findOne({ productId: productIdObject });
-  console.log("Review productId in DB:", review);
+  const matchStage = {
+    productId: productIdObject,
+    isDeleted: false,
+  };
 
-  const user = await User.findOne({ _id: review?.userId });
-  console.log("Review userId in DB:", user);
-
-  const total = await Review.countDocuments();
-
-  const result = await Review.aggregate([
-    {
-      $match: {
-        productId: productIdObject,
-        isDeleted: { $ne: true }
+  const [total, result] = await Promise.all([
+    Review.countDocuments(matchStage),
+    Review.aggregate([
+      { $match: matchStage },
+      {
+        $lookup: {
+          from: "Admin",
+          localField: "userId",
+          foreignField: "_id",
+          as: "userInfo",
+        },
       },
-    },
-    {
-      $lookup: {
-        from: 'Admin',
-        localField: 'userId',
-        foreignField: '_id',
-        as: 'userInfo'
-      }
-    },
-    {
-      $unwind: {
-        path: '$userInfo',
-        preserveNullAndEmptyArrays: true
-      }
-    },
-    {
-      $project: {
-        ratings: 1,
-        attachment: 1,
-        comments: 1,
-        isDeleted: 1,
-        updatedAt: 1,
-        createdAt: 1,
-        'userInfo.userName': 1,
-        'userInfo.profile': 1
-      }
-    },
-    {
-      $limit: limit
-    },
-    { $skip: skip }
+      {
+        $unwind: {
+          path: "$userInfo",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          ratings: 1,
+          attachment: 1,
+          comments: 1,
+          isDeleted: 1,
+          createdAt: 1,
+          updatedAt: 1,
+          "userInfo.userName": 1,
+          "userInfo.profile": 1,
+        },
+      },
+      { $skip: skip },
+      { $limit: limit },
+    ]),
   ]);
-  console.log("Review aggregation result:", result)
+
   return {
     meta: buildMeta(page, limit, total),
     data: result as T[],
-  };;
+  };
 };
+
+// const getReviewService = async <T>(productId: string, query: Record<string, unknown>): Promise<IAggregationResponse<T>> => {
+//   const { page, limit, skip } = calculatePagination(query);
+
+
+//   const productIdObject = await idConverter(productId);
+//   console.log("Converted ProductId: ", productIdObject);
+
+//   const review = await Review.findOne({ productId: productIdObject });
+//   console.log("Review productId in DB:", review);
+
+//   const user = await User.findOne({ _id: review?.userId });
+//   console.log("Review userId in DB:", user);
+
+//   const total = await Review.countDocuments();
+
+//   const result = await Review.aggregate([
+//     {
+//       $match: {
+//         productId: productIdObject,
+//         isDeleted: { $ne: true }
+//       },
+//     },
+//     {
+//       $lookup: {
+//         from: 'Admin',
+//         localField: 'userId',
+//         foreignField: '_id',
+//         as: 'userInfo'
+//       }
+//     },
+//     {
+//       $unwind: {
+//         path: '$userInfo',
+//         preserveNullAndEmptyArrays: true
+//       }
+//     },
+//     {
+//       $project: {
+//         ratings: 1,
+//         attachment: 1,
+//         comments: 1,
+//         isDeleted: 1,
+//         updatedAt: 1,
+//         createdAt: 1,
+//         'userInfo.userName': 1,
+//         'userInfo.profile': 1
+//       }
+//     },
+//     { $skip: skip },
+//     {
+//       $limit: limit
+//     },
+
+//   ]);
+//   console.log("Review aggregation result:", result)
+//   return {
+//     meta: buildMeta(page, limit, total),
+//     data: result as T[],
+//   };;
+// };
 
 // const updateReviewService = async (payload: TAdminUpdate) => {
 //   const { adminId, ...updateData } = payload;
